827f3c0f649547e60e5196c9f0c5077d9229148e
Sachin Ramasamy <blablabpa4@gmail.com>
2025-10-08 12:54:20 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/zoho/',
   plugins: [react()],
-    build: {
-    outDir: 'docs'  
-  }
+   base: '/zoho/',
 })
